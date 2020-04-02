1e9df0a721f9b44cf70fbea33b99c90f14349d68
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-02 03:39:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,11 @@ class Student
     sql = <<-SQL
     SELECT * FROM students 
     WHERE name = ?
+    LIMIT 1
     SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first 
   end
   
   def save
