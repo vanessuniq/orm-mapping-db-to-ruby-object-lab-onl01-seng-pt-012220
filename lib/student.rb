4bbe9921da49c4aec144ec75b6594d7a111bd7e7
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-02 03:55:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,15 @@ class Student
     end
   end
   
-  def self
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = '10th'
+    LIMIT ?
+    SQL
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+  end
   
   def save
     sql = <<-SQL
