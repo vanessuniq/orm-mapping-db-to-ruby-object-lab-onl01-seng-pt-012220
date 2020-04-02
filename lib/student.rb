8ce168583bc4f98f60912720f81264ebe4821d34
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-02 02:57:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    students
+    new_student = self.new
     # create a new Student object given a row from the database
   end
 
