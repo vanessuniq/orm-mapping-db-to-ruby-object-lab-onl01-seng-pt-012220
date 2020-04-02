39afc564c2808a012a0a2f83b5d9715add4a559d
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-02 03:51:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,17 @@ class Student
     end
   end
   
-  def self.
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade != '12th'
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self
   
   def save
     sql = <<-SQL
